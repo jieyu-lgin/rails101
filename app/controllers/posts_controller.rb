39e5c68281390b9ba42a4631b953d382663389f3
jieyu-lgin <jieyu_lgin@sina.com>
2017-04-28 17:14:27 +0800

    use simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,15 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @group = Group.find(params[:group_id])
+
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "Post deleted"
+    redirect_to group_posts_path
+  end
+
   private
 
   def post_params
